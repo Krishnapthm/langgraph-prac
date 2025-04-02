62bf9d011b4a1d85de46486c6c6ab888f7e068bc
Krishnapatham <82874782+Krishnapthm@users.noreply.github.com>
2025-04-02 15:45:33 +0000

    final

--- reflection/basic.py
+++ reflection/basic.py
@@ -5,3 +5,41 @@ from langgraph.graph import END, MessageGraph
 from chains import generation_chain, reflection_chain
 
 load_dotenv()
+
+graph = MessageGraph()
+
+GENERATE = 'generate'
+REFLECT = 'reflect'
+
+def generate_node(state):
+    return generation_chain.invoke({
+        "messages": state
+    })
+
+def reflect_node(state):
+    response = reflection_chain.invoke({
+        "messages": state
+    })
+
+    return [HumanMessage(content=response.content)]
+
+graph.add_node(GENERATE, generate_node)
+graph.add_node(REFLECT, reflect_node)
+graph.set_entry_point(GENERATE)
+
+def should_continue(state):
+    if(len(state)>6):
+        return END
+    return REFLECT
+
+graph.add_conditional_edges(GENERATE, should_continue)
+
+graph.add_edge(REFLECT, GENERATE)
+
+app = graph.compile()
+
+print(app.get_graph().draw_mermaid())
+app.get_graph().print_ascii()
+
+response = app.invoke(HumanMessage(content="AI agents taking over content creation"))
+print(response)
--- reflection/chains.py
+++ reflection/chains.py
@@ -1,31 +1,30 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-from langchain.agents import initialize_agent, tool
-from langchain_community.tools import TavilySearchResults
+from langchain_openai import AzureChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-import datetime
+import dotenv
+
+dotenv.load_dotenv()
 
 
 generation_prompt = ChatPromptTemplate.from_messages([
-    {
+    (
         "system",
         "you are a twitter techie influencer assitnt tasked with writing excellent twitter posts."
         "generate the best twitter post possible for the user's request."
         "if the user provides a cirtique, respont witht a revised version of your previous attmepts"
-    },
+    ),
     MessagesPlaceholder(variable_name="messages"),
 ])
 
 reflection_prompt = ChatPromptTemplate.from_messages([
-    {
+    (
         "system",
         "you are a twitter techie influencer grading a tweet. generate critique and recommnedations for the users tweet"
         "always provide detailed recomendations, including reuqests for length, virality, style etc"
-    },
+    ),
     MessagesPlaceholder(variable_name="messages"),
 ])
 
-llm=ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+llm=AzureChatOpenAI(model="gpt-4o-mini", api_version="2024-12-01-preview")
 
 generation_chain = generation_prompt | llm
 reflection_chain = reflection_prompt | llm
